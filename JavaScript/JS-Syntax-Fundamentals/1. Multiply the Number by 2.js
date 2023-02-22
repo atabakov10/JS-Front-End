5229bca05c22d45a1b66dfe5728402acac6e57d5
ANGEL <atabakov99@abv.bg>
2023-02-22 15:58:46 +0200

    Exercises from lab for judge

---
+++ JavaScript/JS-Syntax-Fundamentals/1. Multiply the Number by 2.js
@@ -0,0 +1,7 @@
+function solve(num) {
+    num = num * 2;
+    console.log(num)
+}
+solve(2);
+solve(5);
+solve(20);
---
+++ JavaScript/JS-Syntax-Fundamentals/3. Excellent Grade.js
@@ -0,0 +1,10 @@
+function solve(gradeNum) {
+    if (gradeNum >= 5.50){
+        console.log('Excellent');
+    }
+    else {
+        console.log('Not excellent')
+    }
+}
+
+solve(5.49);
