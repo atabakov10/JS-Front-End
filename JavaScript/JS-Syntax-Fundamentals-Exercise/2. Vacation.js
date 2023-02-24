a23cc8c85f5454a9eda988b5cc05b0e9c9f0f82e
ANGEL <atabakov99@abv.bg>
2023-02-24 03:30:51 +0200

    Exercises JS-Fund

--- JavaScript/JS-Syntax-Fundamentals-Exercise/2. Vacation.js
+++ JavaScript/JS-Syntax-Fundamentals-Exercise/2. Vacation.js
@@ -1,112 +1,62 @@
 function solve(people, type, dayOfWeek) {
     let price = 0;
     let discount = 0;
-    if(type == 'Students'){
-        if(dayOfWeek == 'Friday'){
+    if(type === 'Students'){
+        if(dayOfWeek === 'Friday'){
             price = 8.45 * people;
             if(people >= 30){
                 discount = 0.15;
                 price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
             }
         }
-        else if(dayOfWeek == 'Saturday'){
+        else if(dayOfWeek === 'Saturday'){
             price = 9.80 * people;
             if(people >= 30){
                 discount = 0.15;
                 price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
             }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
+           
         }
-        else if(dayOfWeek == 'Sunday'){
+        else if(dayOfWeek === 'Sunday'){
             price = 10.46 * people;
             if(people >= 30){
                 discount = 0.15;
                 price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
             }
         }
 
     }
-    else if(type == 'Business'){
-        if(dayOfWeek == 'Friday'){
+    else if(type === 'Business'){
+        if(people === 100){
+            people = people - 10;
+        }
+
+        if(dayOfWeek === 'Friday'){
             price = 10.90 * people;
-            if(people == 100){
-                discount = 0.10;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
         }
-        else if(dayOfWeek == 'Saturday'){
+        else if(dayOfWeek === 'Saturday'){
             price = 15.60 * people;
-            if(people == 100){
-                discount = 0.10;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
         }
-        else if(dayOfWeek == 'Sunday'){
+        else if(dayOfWeek === 'Sunday'){
             price = 16 * people;
-            if(people == 100){
-                discount = 0.10;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
         }
     }
-    else if(type == 'Regular'){
-        if(dayOfWeek == 'Friday'){
+    else if(type === 'Regular'){
+        if(dayOfWeek === 'Friday'){
             price = 15 * people;
-            if(people >= 10 && people <= 20){
-                discount = 0.05;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
         }
-        else  if(dayOfWeek == 'Saturday'){
+        else if(dayOfWeek === 'Saturday'){
             price = 20 * people;
-            if(people >= 10 && people <= 20){
-                discount = 0.05;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
         }
-        else  if(dayOfWeek == 'Sunday'){
+        else if(dayOfWeek === 'Sunday'){
             price = 22.50 * people;
-            if(people >= 10 && people <= 20){
-                discount = 0.05;
-                price = price - (price * discount);
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
-            else{
-                console.log(`Total price: ${price.toFixed(2)}`);
-            }
+        }
+
+        if(people >= 10 && people <= 20){
+            price -= price * 0.05;
         }
     }
+    console.log(`Total price: ${price.toFixed(2)}`);
 }
 solve(40,
 
