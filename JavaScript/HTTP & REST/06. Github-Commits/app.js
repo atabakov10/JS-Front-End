f76c49eb49c9196339d0e93d192182cf5efdd71e
ANGEL <atabakov99@abv.bg>
2023-03-24 01:00:19 +0200

    HTTP-and-REST

---
+++ JavaScript/HTTP & REST/06. Github-Commits/app.js
@@ -0,0 +1,28 @@
+async function loadCommits() {
+  const BASE_URL = "https://api.github.com/repos/";
+  const username = document.getElementById("username");
+  const repo = document.getElementById("repo");
+  const commits = document.getElementById("commits");
+  const loader = document.getElementById("loader");
+  const usernameVal = username.value;
+  const repoVal = repo.value;
+
+  try {
+    loader.style.display = "block";
+    const allCommitsRes = await fetch(
+      `${BASE_URL}${usernameVal}/${repoVal}/commits`
+    );
+    const data = await allCommitsRes.json();
+
+    loader.style.display = "none";
+    data.forEach(({ commit }) => {
+      const li = document.createElement("li");
+      li.textContent = `${commit.author.name} ${commit.message}`;
+      commits.appendChild(li);
+    });
+  } catch (err) {
+    const li = document.createElement("li");
+    li.textContent = err.message;
+    commits.appendChild(li);
+  }
+}
